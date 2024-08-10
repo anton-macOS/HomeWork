c9e3a0262a48e54306ad59be56ea33745c4132e5
Anton <anton.kiriliuk@gmail.com>
2024-08-11 00:23:30 +0300

    comments have been corrected

--- API/api.py
+++ API/api.py
@@ -7,22 +7,50 @@ class CurrencyConvertor:
         self.key = key
         self.url = f'https://v6.exchangerate-api.com/v6/{self.key}'
 
+    @staticmethod
+    def check_response(url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            return data
+
+        except requests.exceptions.ConnectionError:
+            return 'Ошибка соединения'
+
+        except requests.exceptions.HTTPError:
+            return 'HTTP ошибка'
+
+        except requests.exceptions.Timeout:
+            return 'Ошибка времени ожидания'
+
+        except requests.exceptions.RequestException as e:
+            return f'Произошла ошибка - {e}'
+
+        except requests.exceptions.JSONDecodeError:
+            return 'Вернулся не JSON'
+
+        except Exception as e:
+            return f'Ошибка - {e}'
+
     def get_exchange_info(self, from_curr, to_curr):
-        responce = requests.get(f'{self.url}/pair/{from_curr}/{to_curr}')
-        data = responce.json()
-        if data['result'] == 'success':
-            return f'Вот актальный курс на данный момент: {data['conversion_rate']}'
-        elif data['result'] == 'error':
-            return f'Что-то пошло не так! Вот код ошибки: {data['error-type']}'
+        url = f'{self.url}/pair/{from_curr}/{to_curr}'
+        data = self.check_response(url)
+        if isinstance(data, dict):
+            if data['result'] == 'success':
+                return f'Вот актальный курс на данный момент: {data['conversion_rate']}'
+            elif data['result'] == 'error':
+                return f'Что-то пошло не так! Вот код ошибки: {data['error-type']}'
 
     def convertor(self, from_curr, to_curr, value):
-        responce = requests.get(f'{self.url}/pair/{from_curr}/{to_curr}/{value}')
-        data = responce.json()
-        if data['result'] == 'success':
-            return (f'Актуальный курс обмена - {data['conversion_rate']}, '
-                    f' вы получите на руки - {data['conversion_result']}')
-        elif data['result'] == 'error':
-            return f'Что-то пошло не так! Вот код ошибки: {data['error-type']}'
+        url = f'{self.url}/pair/{from_curr}/{to_curr}/{value}'
+        data = self.check_response(url)
+        if isinstance(data, dict):
+            if data['result'] == 'success':
+                return (f'Актуальный курс обмена - {data['conversion_rate']}, '
+                        f' вы получите на руки - {data['conversion_result']}')
+            elif data['result'] == 'error':
+                return f'Что-то пошло не так! Вот код ошибки: {data['error-type']}'
 
 
 convertor = CurrencyConvertor(api_key)
